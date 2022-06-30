d2a27f85bec23c4ba6065ad853060f4726dd5f0f
MicroPanda123 <dawid12345.dawid@gmail.com>
2022-06-30 22:00:29 +0200

    Turned json database into sql one, api is practically the same but I think it's more stable that way than reading files.

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from typing import Union
 from fastapi import FastAPI, Response
 import json
 import random
+import sqlite3
 #####################
 
 ## Main Code ##
 app = FastAPI()
+conn = sqlite3.connect('json.db')
 
 @app.get("/")
 def read_root(response: Response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     number = random.randint(1,29)
-    j = open('./json/' + str(number) + '.json')
-    img = json.load(j)
+    cur = conn.execute(f"SELECT json FROM jsons WHERE ID={number};")
+    img = json.load(cur.fetchall()[0])
     return img
 
 print("Server is running...")
